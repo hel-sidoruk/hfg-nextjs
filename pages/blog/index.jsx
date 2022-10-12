030d9c21233e9e0ea726638c3ecc618b4a00b32c
@olga_sidoruk <olya.sidoruk98@gmail.com>
2022-10-12 21:50:25 +0300

    pagination

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -11,30 +11,30 @@ import { getPagesArray } from '../../utils/pages';
 import styles from '../../styles/blog.module.scss';
 import ArticlesSkeleton from '../../components/UI/ArticlesSkeleton';
 
-export default function BlogPage() {
-  // const router = useRouter();
-  // const [isLoading, setLoading] = useState(false);
-  // const startLoading = () => setLoading(true);
-  // const stopLoading = () => setLoading(false);
-  // const changePage = (page) => {
-  //   const currentPath = router.pathname;
-  //   const currentQuery = router.query;
-  //   currentQuery.page = page;
-  //   router.push({
-  //     pathname: currentPath,
-  //     query: currentQuery,
-  //   });
-  // };
+export default function BlogPage({ posts, currentPage, limit, pageCount }) {
+  const router = useRouter();
+  const [isLoading, setLoading] = useState(false);
+  const startLoading = () => setLoading(true);
+  const stopLoading = () => setLoading(false);
+  const changePage = (page) => {
+    const currentPath = router.pathname;
+    const currentQuery = router.query;
+    currentQuery.page = page;
+    router.push({
+      pathname: currentPath,
+      query: currentQuery,
+    });
+  };
 
-  // useEffect(() => {
-  //   router.events.on('routeChangeStart', startLoading);
-  //   router.events.on('routeChangeComplete', stopLoading);
-  //   return () => {
-  //     router.events.off('routeChangeStart', startLoading);
-  //     router.events.off('routeChangeComplete', stopLoading);
-  //   };
-  // });
-  // const pagesArray = getPagesArray(pageCount);
+  useEffect(() => {
+    router.events.on('routeChangeStart', startLoading);
+    router.events.on('routeChangeComplete', stopLoading);
+    return () => {
+      router.events.off('routeChangeStart', startLoading);
+      router.events.off('routeChangeComplete', stopLoading);
+    };
+  });
+  const pagesArray = getPagesArray(pageCount);
 
   return (
     <>
@@ -44,7 +44,7 @@ export default function BlogPage() {
       <Background page={'blog-page'} />
       <Section sectionName={styles.articles}>
         <Title variant={'align-center'}>Блог</Title>
-        {/* <div className={styles.content}>
+        <div className={styles.content}>
           {isLoading ? (
             <ArticlesSkeleton num={limit} styles={styles} />
           ) : (
@@ -55,16 +55,16 @@ export default function BlogPage() {
             page={currentPage}
             changePage={changePage}
           />
-        </div> */}
+        </div>
       </Section>
     </>
   );
 }
-// BlogPage.getInitialProps = async ({ query }) => {
-//   const page = query.page || 1;
-//   const { data } = await axios.get(
-//     `${process.env.NEXT_PUBLIC_HOST}/api/articles?page=${page}&limit=6`
-//   );
-//   const { posts, pageCount, limit, currentPage } = data;
-//   return { posts, pageCount, limit, currentPage };
-// };
+BlogPage.getInitialProps = async ({ query }) => {
+  const page = query.page || 1;
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_HOST}/api/articles?page=${page}&limit=6`
+  );
+  const { posts, pageCount, limit, currentPage } = data;
+  return { posts, pageCount, limit, currentPage };
+};
