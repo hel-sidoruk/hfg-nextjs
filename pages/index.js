9ced4d621243edc5844ed2e0bd70ba5ac24fdc2b
@olga_sidoruk <olya.sidoruk98@gmail.com>
2022-10-13 17:38:25 +0300

    manifest

--- pages/index.js
+++ pages/index.js
@@ -27,24 +27,10 @@ export default function Home({ recentArticles }) {
     <>
       <Head>
         <title>Holy Family Gym | Зал единоборств в Минске</title>
-        <meta name="theme-color" content="#1F1A1A" />
         <meta
           name="description"
           content="Тренировки по Бразильскому джиу-джитсу, Грэпплингу, Вольной борьбе, Тайскому боксу, ММА. Единоборства в Минске для детей и взрослых. Бразильское джиу-джитсу и грэпплинг Минск. Тайский бокс Минск. ММА Минск. Вольная борьба Минск"
         />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://hfg-nextjs.vercel.app/" />
-        <meta
-          property="og:title"
-          content="Holy Family Gym | Зал единоборств в Минске"
-        />
-        <meta
-          property="og:description"
-          content="Тренировки по Бразильскому джиу-джитсу, Грэпплингу, Вольной борьбе, Тайскому боксу, ММА. Единоборства в Минске для детей и взрослых. Бразильское джиу-джитсу и грэпплинг Минск. Тайский бокс Минск. ММА Минск. Вольная борьба Минск"
-        />
-        <meta property="og:image" content="/big-logo.webp" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/maskable_icon.png" />
       </Head>
       <Background page={'home-page'} />
       <Hero home={true} />
